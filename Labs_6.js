89d422329b543c5b46add6238693285754d6fdf5
Ilya <ilyaphotoshop17@gmail.com>
2020-09-16 15:24:10 +0300

    [add] added new Labs_6

---
+++ Labs_6.js
@@ -0,0 +1,63 @@
+
+// #1
+
+class GetNameDataWord {
+    constructor(args = '') {
+        this.args = args
+    }
+
+    getArgs(args) {
+        let arg = this.args = args
+        const data = new Date().getDate()
+        const words = ['Hallo, wie gehts','Mir gehts gut', 'Freut mich', 'Vielen Dank.']
+        let refreshWords = words.sort(() => 0.5 - Math.random())
+        console.log(`Name author: ${arg}, Data: ${data}, Words: ${refreshWords}`)
+    }
+}
+
+const getNameDataWord = new GetNameDataWord()
+getNameDataWord.getArgs('Bogdan')
+
+
+// #2
+
+class setIntToString {
+    constructor(int = 0.5) {
+        this.int = int
+    }
+
+    getIntToStringMeth() {
+        let toString = this.int.toString()
+        console.log('Int to String:', toString)
+    }
+
+    getRandomWords(num) {
+        const words = ['Hallo, wie gehts','Mir gehts gut', 'Freut mich', 'Vielen Dank.']
+        let random = words.sort(() => num - Math.random())
+        console.log('От случайного n', random)
+    }
+}
+
+const setIntToStr = new setIntToString()
+setIntToStr.getRandomWords(0.5)
+
+// #3
+
+class putStringToString {
+    constructor(str = 'Words') {
+        this.str = str
+    }
+
+    getReversMeth() {
+        let words = this.str
+        const rever = words
+            .split('')
+            .reverse()
+            .join('')
+            .trim()
+        console.log('Reverse: ', rever)
+    }
+}
+
+const putStr = new putStringToString()
+putStr.getReversMeth()
